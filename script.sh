7f94e58749502e7833da90abdf41d0c911a8b87b
naythuzaypro2019 <144746675+naythuzaypro2019@users.noreply.github.com>
2023-09-12 13:04:41 +0630

    Update script.sh

--- script.sh
+++ script.sh
@@ -33,8 +33,8 @@ echo "
 
 <h3><font color='red'>▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬
 </font></h3>" | tee /etc/ssh/gcp_404 >/dev/null
-useradd "404" --shell=/bin/false -M
-echo "404:404" | chpasswd
+useradd "444" --shell=/bin/false -M
+echo "444:444" | chpasswd
 
 echo ""
 
